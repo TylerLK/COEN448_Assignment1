464cba05a318335f24d4b09cd46b09e6061c0112
Sunil Kublalsingh <hulksunil@hotmail.com>
2026-02-14 21:10:17 -0500

    Merged old + new MicroService classes. We have a string serviceId so we need to take care of that in the tests.

--- src/main/java/coen448/computablefuture/test/Microservice.java
+++ src/main/java/coen448/computablefuture/test/Microservice.java
@@ -1,17 +1,34 @@
 package coen448.computablefuture.test;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 class Microservice {
-    // Successful Microservice Execution
-	public CompletableFuture<String> retrieveAsync(String input) {
-        return CompletableFuture.supplyAsync(() -> input.toUpperCase());
+    private final String serviceId;
+
+    public Microservice(String serviceId) {
+        this.serviceId = serviceId;
     }
-    
+
+    // Successful Microservice Execution
+    public CompletableFuture<String> retrieveAsync(String input) {
+        return CompletableFuture.supplyAsync(() -> {
+            // jitter: 0..30ms to perturb scheduling
+            int delayMs = ThreadLocalRandom.current().nextInt(0, 31);
+            try {
+                TimeUnit.MILLISECONDS.sleep(delayMs);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+
+            return serviceId + ":" + input.toUpperCase();
+        });
+    }
+
     // Failed Microservice Execution
-	public CompletableFuture<String> retrieveAsyncFail(String error) {
-		return CompletableFuture.supplyAsync(() -> {
-			throw new RuntimeException(error);
-		});
-	}
+    public CompletableFuture<String> retrieveAsyncFail(String error) {
+        return CompletableFuture.supplyAsync(() -> { 
+            throw new RuntimeException(error); 
+        });
+    }
 }
